1eb260efdab3497a0c149244cce6e11458e03e0d
amp <ampanahi20@gmail.com>
2021-06-17 18:02:19 +0430

    first commit

---
+++ src/constants/eggPriceContracts.ts
@@ -0,0 +1,7 @@
+const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
+  cakeAddress: '0x8F8a854865c7B16F75f2b8f251475558045D29A3',
+  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+  lpAddress: '0xbdfb8e9Bc83Fa427932B0EE272c6E1Cc1551dB36'
+}
+
+export default priceContracts
